29d12f98adedfb4c8253f5df1816bbc79cc7d30a
user test <klimchuk.alieksandr.1987@mail.ru>
2023-07-04 21:09:41 +0300

    first commit

--- untitled/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int x = 11;
-        while (x < 13) {
-            System.out.print("HEllo, " + x);
-            x = x + 10;
-        }
-    }
-}
